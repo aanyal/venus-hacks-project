3e18288dabcba391153fad662aced0aa05908357
aanyal <aanyalakhani01@gmail.com>
2026-05-16 12:22:30 -0700

    Initial Commit

---
+++ VenusHacksProject/VenusHacksProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenusHacksProjectApp.swift
+//  VenusHacksProject
+//
+//  Created by Aanya L on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct VenusHacksProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
